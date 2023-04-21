191ca68033c59547764358fc10ff0ace67910934
Itssogeking <128600664+Itssogeking@users.noreply.github.com>
2023-04-21 11:40:47 +0100

    Update Streamlit_app.py
    Created a function called get_fruityvice_data  - which basically carries out the part of the search where the users chosen fruit goes to fruityvice, and normalises that response 
    
    means when my try is carried out it just uses the function in combination with their input to then display the dataframe/ table

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -27,16 +27,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
-
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) #import requests - moved all imports to the top of the page
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it 
+    return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #import requests - moved all imports to the top of the page
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it 
-    streamlit.dataframe(fruityvice_normalized) #outputting the normalised json file as a table
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function) #outputting the normalised json file as a table
     
 except URLError as e:
     streamlit.error()
