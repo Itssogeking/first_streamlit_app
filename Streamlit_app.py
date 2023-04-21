b3ab29ce26c12dfc82c5ef7421ed32c1c61b8c7d
Itssogeking <128600664+Itssogeking@users.noreply.github.com>
2023-04-21 11:30:29 +0100

    Update Streamlit_app.py
    Tidied up the section around Fruityvice to isolate code that is loaded once from code that needs to be loaded multiple times

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -25,19 +25,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
 #new section to display fruityvice api response
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice;
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #import requests - moved all imports to the top of the page
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it 
+    streamlit.dataframe(fruityvice_normalized) #outputting the normalised json file as a table
+    
+except URLError as e:
+    streamlit.error()
 
-#import requests - moved all imports to the top of the page
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()) - just pastes the json value on the screen
 
-#take the json version of the response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#outputting the normalised json file as a table
-streamlit.dataframe(fruityvice_normalized)
 #don't run anything past here whilie we troubleshoot
 streamlit.stop()
 
